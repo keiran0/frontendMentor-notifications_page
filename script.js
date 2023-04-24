3d2b0c5c8b5b89e7c4ddeb2a08d3302540880f71
Keiran <keinn2210@gmail.com>
2023-04-25 01:22:23 +0800

    commit3

--- script.js
+++ script.js
@@ -6,12 +6,6 @@ const dot = document.createElement("span");
 dot.innerHTML = "&#9679";
 dot.classList.add("red-dot");
 
-function addUnreadDot() {
-    for (var i=0; i<unreadList.length; i++) {
-        document.querySelectorAll(".time")[i].parentNode.insertBefore(dot.cloneNode(true),document.querySelectorAll(".time")[i]);
-    }
-}
-
 function checkNotificationCount() {
     var unreadNotifications = 0
     for (var i=0; i<numberOfNotifications; i++) {
@@ -22,24 +16,40 @@ function checkNotificationCount() {
     document.querySelector(".notification-count").textContent = unreadNotifications;
 }
 
+// if notification has unread in classlist, add red dot
+function addUnreadDot() {
+    for (var i=0; i<notificationList.length; i++) {
+        if (notificationList[i].classList.contains("unread")) {
+            document.querySelectorAll(".time")[i].parentNode.insertBefore(dot.cloneNode(true),document.querySelectorAll(".time")[i]);
+        }
+        
+    }
+}
+
 for (var i=0; i<numberOfNotifications; i++) {
     notificationList[i].addEventListener("click", function() {
         this.classList.remove("unread");
         this.querySelector(".red-dot").remove();
         checkNotificationCount();
     })
+    checkNotificationCount();
 }
 
-
-
-document.querySelector(".mark-as-read").addEventListener("click", function(){
+document.querySelector(".mark-as-read").addEventListener("click", function() {
     for (var i=0; i<numberOfNotifications; i++) {
-        document.querySelectorAll(".notification")[i].classList.remove("unread");
-        document.querySelectorAll(".red-dot")[0].remove();
-        checkNotificationCount();
-        }
-    
+        console.log(i);
+        try {
+            document.querySelectorAll(".notification")[i].classList.remove("unread");
+        } catch (err) {}
+        
+        try {
+            document.querySelectorAll(".red-dot")[0].remove();
+        } catch (err) {}
+    }
+    checkNotificationCount();
 })
 
+
+
 addUnreadDot();
-// document.querySelectorAll(".notification").getAttribute()
+checkNotificationCount();
